01e8ab79013332e7dd5153d6a4725b971f187800
Wes Lloyd <wlloyd@uw.edu>
2018-11-01 20:20:30 -0700

    update

--- faas_inspector/lambda/java_template/test/callservice.sh
+++ faas_inspector/lambda/java_template/test/callservice.sh
@@ -4,21 +4,19 @@
 json={"\"name\"":"\"Fred\u0020Smith\",\"param1\"":1,\"param2\"":2,\"param3\"":3}
 
 echo "Invoking Lambda function using API Gateway"
-#time output=`curl -s -H "Content-Type: application/json" -X POST -d  $json https://0h20avome2.execute-api.us-east-1.amazonaws.com/dbtest`
-#time output=`curl -s -H "Content-Type: application/json" -X POST -d  $json https://g8ehey0fmc.execute-api.us-east-1.amazonaws.com/helloSQLite` 
+time output=`curl -s -H "Content-Type: application/json" -X POST -d  $json {INSERT API GATEWAY URL HERE}`
 
 echo ""
 echo "CURL RESULT:"
-echo $output | jq
+echo $output
 echo ""
 echo ""
 
 echo "Invoking Lambda function using AWS CLI"
-#time output=`aws lambda invoke --invocation-type RequestResponse --function-name dbtest --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name helloSqlite --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo` 
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name {INSERT AWS FUNCTION NAME HERE} --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
 echo ""
 echo "AWS CLI RESULT:"
-echo $output | jq
+echo $output
 echo ""
 
 
